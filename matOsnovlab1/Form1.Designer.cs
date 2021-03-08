d805340c6037fbcef587c1ef13941319982e13e4
RS <sadikov.tj@yandex.ru>
2021-03-08 20:46:50 +0300

    Добавил 2 задание

--- matOsnovlab1/Form1.Designer.cs
+++ matOsnovlab1/Form1.Designer.cs
@@ -29,29 +29,31 @@ namespace matOsnovlab1
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
             ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
             this.SuspendLayout();
             // 
             // chart2
             // 
-            chartArea1.Name = "ChartArea1";
-            this.chart2.ChartAreas.Add(chartArea1);
-            this.chart2.Location = new System.Drawing.Point(-6, 0);
+            chartArea3.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea3);
+            this.chart2.Location = new System.Drawing.Point(11, 41);
             this.chart2.Name = "chart2";
-            series1.ChartArea = "ChartArea1";
-            series1.Name = "Series1";
-            this.chart2.Series.Add(series1);
-            this.chart2.Size = new System.Drawing.Size(998, 648);
+            series3.ChartArea = "ChartArea1";
+            series3.Name = "Series1";
+            this.chart2.Series.Add(series3);
+            this.chart2.Size = new System.Drawing.Size(1204, 746);
             this.chart2.TabIndex = 0;
             this.chart2.Text = "chart2";
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(8, 644);
+            this.button2.Location = new System.Drawing.Point(11, 13);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 1;
@@ -59,10 +61,29 @@ namespace matOsnovlab1
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(93, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(247, 13);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(458, 23);
+            this.progressBar1.TabIndex = 3;
+            // 
             // Form1
             // 
             this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.ClientSize = new System.Drawing.Size(991, 675);
+            this.ClientSize = new System.Drawing.Size(1227, 799);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.chart2);
             this.Name = "Form1";
@@ -77,6 +98,8 @@ namespace matOsnovlab1
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ProgressBar progressBar1;
     }
 }
 
--- matOsnovlab1/Form1.cs
+++ matOsnovlab1/Form1.cs
@@ -7,11 +7,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace matOsnovlab1
 {
+     
     public partial class Form1 : Form
     {
+        double Function(double x, double a)
+        {
+            return a * (x - x * x);
+        }
         public Form1()
         {
             InitializeComponent();
@@ -22,6 +28,9 @@ namespace matOsnovlab1
             chart2.Series[2].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
             chart2.ChartAreas[0].AxisX.MajorGrid.Enabled = false;
             chart2.ChartAreas[0].AxisY.MajorGrid.Enabled = false;
+
+            chart2.Series.Add("serise3");
+            chart2.Series[3].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
 
 
         }
@@ -35,7 +44,7 @@ namespace matOsnovlab1
                 double x = 0.001;
                 double y = 0.001;
                 double y2 = 0.001;
-                // double a = 2.8;
+                // double a = 2.8;123123
 
 
                 this.chart2.Series[0].Points.Clear();
@@ -44,7 +53,8 @@ namespace matOsnovlab1
 
                 while (x < 1)
                 {
-                    y = a[r1] * (x - x * x);
+                    y = Function(x, a[r1]);
+                    //y = a[r1] * (x - x * x);
                     x = x + 0.001;
                     chart2.Series[0].Points.AddXY(x, y); //Рисуем параболу
                 }
@@ -66,7 +76,8 @@ namespace matOsnovlab1
                 chart2.Series[2].Color = Color.Lime;
                 for (int i = 0; i < 10; i += 1)
                 {
-                    y = a[r1] * (x - x * x);
+                    y = Function(x, a[r1]);
+                   // y = a[r1] * (x - x * x);
 
                     chart2.Series[2].Points.AddXY(x, y); //Рисуем паутину
 
@@ -77,6 +88,50 @@ namespace matOsnovlab1
                 chart2.SaveImage(testStr, System.Drawing.Imaging.ImageFormat.Jpeg);
                
             }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            double x = 0.001;
+            double y;
+            // double a = 1;
+            double aDef;
+            //this.chart2.ChartAreas[3].AxisY.Minimum = 2;
+
+           
+            double[] a2 = { 0.5, 1, 1.3, 1.5, 2 };
+
+            for (int r1 = 0; r1 < a2.Length; r1 += 1)
+            {
+                progressBar1.Maximum += Convert.ToInt32(((4-a2[r1])/0.00001));
+            }
+
+            progressBar1.Value = 0;
+
+            for (int r1 = 0; r1 < a2.Length; r1 += 1)
+            {
+
+                this.chart2.Series[3].Points.Clear();
+                this.chart2.Series[3].MarkerSize = 2;
+                aDef = a2[r1];
+                y = x;
+                while (a2[r1] < 4)
+                {
+                    x = y;
+                    y = Function(x, a2[r1]);
+                    if (a2[r1] > (aDef + 1.5))
+                    {
+                        chart2.Series[3].Points.AddXY(a2[r1], x); //Рисуем параболу
+                        //Thread.Sleep(50);
+                    }
+                    a2[r1] += 0.00001;
+
+                    progressBar1.Value += 1;
+                }
+                string testStr = "data/" + r1 + "aZadan2.jpeg";
+                chart2.SaveImage(testStr, System.Drawing.Imaging.ImageFormat.Jpeg);
+            }
+
         }
     }
 }
